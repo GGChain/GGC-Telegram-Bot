4b34ca812cc35355135bb700f02ebecaddb72054
Atruo <banyuls98@gmail.com>
2018-09-18 19:11:56 +0200

    More BBDD

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 process.env["NTBA_FIX_319"] = 1;
 console.log('Starting Bot...');
 var TelegramBot = require('node-telegram-bot-api');
-
+var mysql = require('mysql');
 var token = 'key';
+
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -14,11 +15,40 @@ const bot = new TelegramBot(token, {polling: true});
   let chatId = msg.chat.id;
   var command = msg.text.toString().toLowerCase();
   var args ='';
-  //BBD
-  let jsonSql = require('json-sql');
-  const promise = require('bluebird');
-  const SqlConnection = require("tedious").Connection;
-  const Request = require("tedious").Request;
+//BBDD
+  var con = mysql.createConnection({
+    host: "host",
+    user: "user",
+    password: "psw",
+    database: "BBDD"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    // con.query("SELECT user_login, user_email FROM wp_users", function (err, result, fields) {
+    //   if (err) throw err;
+    //   console.log('Usuario Base de Datos:');
+    //
+    //   for (var i = 0; i < result.length; i++) {
+    //
+    //     console.log(result[i].user_login+' // '+result[i].user_email);
+    //   }
+    //
+    //   con.end();
+    // });
+    ///////////////////
+    //Borrar
+    // if (err) throw err;
+    //   var sql = "DELETE FROM telegram_users WHERE Nombre = 'Admin'";
+    //   con.query(sql, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("Number of records deleted: " + result.affectedRows);
+    //   });
+
+  });
+
+
+///BBDD
   try {
     if (command === '/register') { // because its imposible to create a file starting with '/'
       command = 'register';
@@ -35,16 +65,17 @@ const bot = new TelegramBot(token, {polling: true});
       commandFile.run(bot, msg ,chatId,args);
     }
 
-  }catch (err) {}
+  }catch (err) {
+  }
  });
 
 
 bot.onText(/\/start/, (msg) => {
 
-bot.sendMessage(msg.chat.id, "Welcome", {
-    "reply_markup": {
-        "keyboard": [["Website", "Twitter"],   ["News", "Bitcointalk"], ["Register"]]
-        }
-    });
+  bot.sendMessage(msg.chat.id, "Welcome", {
+      "reply_markup": {
+          "keyboard": [["Website", "Twitter"],   ["News", "Bitcointalk"], ["Register"]]
+          }
+      });
 
 });
