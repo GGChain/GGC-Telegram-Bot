19219c4e037c01f462ee38fe3b6b23430cb112a8
Atruo <33220763+atruo@users.noreply.github.com>
2018-09-20 15:14:50 +0200

    Some changes

--- comandos/register.js
+++ comandos/register.js
@@ -3,7 +3,7 @@ exports.run = (bot, msg, chatId,args) => {
   if(args === 'x'){
     bot.sendMessage(chatId, '@'+msg.from.username+' Lets talk in private so no one can see us 👀');
     if (localStorage.getItem('launch') === 0 || localStorage.getItem('launch') === '0'  ) {
-      bot.sendMessage(msg.from.id, '🚀Launch Telegram Open registrations:\nFriday 21 September 2018 | 17:00 CMT+1\n\n📝1 Resgister = 1 GGCoin ($GGC)\n🎉First 100 Resgistrations 5 $GGC\n🛄 Max: 5000 $GGC')
+      bot.sendMessage(msg.from.id, '🚀Launch Telegram Open registrations:\nFriday 21 September 2018 | 17:00 CMT+1\n\n📝1 Resgister = 1 GGCoin ($GGC)\n🎉First 100 Registrations 5 $GGC\n🛄 Max: 5000 $GGC')
     }else {
       bot.sendMessage(msg.from.id, "Hey "+msg.from.first_name+", I hear you want to register", {
       "reply_markup": {
@@ -14,7 +14,7 @@ exports.run = (bot, msg, chatId,args) => {
 
   }else{
     if (localStorage.getItem('launch') === 0 || localStorage.getItem('launch') === '0' ) {
-      bot.sendMessage(msg.from.id, '🚀Launch Telegram Open registrations:\nFriday 21 September 2018 | 17:00 CMT+1\n\n📝1 Resgister = 1 GGCoin ($GGC)\n🎉First 100 Resgistrations 5 $GGC\n🛄 Max: 5000 $GGC')
+      bot.sendMessage(msg.from.id, '🚀Launch Telegram Open registrations:\nFriday 21 September 2018 | 17:00 CMT+1\n\n📝1 Resgister = 1 GGCoin ($GGC)\n🎉First 100 Resgistrations 5 $GGC\n🛄 Max: 5000 Resgistrations')
     }else {
       bot.sendMessage(msg.from.id, "Hey "+msg.from.first_name+", I hear you want to register", {
       "reply_markup": {
