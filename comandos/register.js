c2e70ae30c51b92b5bba48dccb042eef02d09112
Atruo <33220763+atruo@users.noreply.github.com>
2018-09-20 15:18:55 +0200

    More changes

--- comandos/register.js
+++ comandos/register.js
@@ -14,7 +14,7 @@ exports.run = (bot, msg, chatId,args) => {
 
   }else{
     if (localStorage.getItem('launch') === 0 || localStorage.getItem('launch') === '0' ) {
-      bot.sendMessage(msg.from.id, '🚀Launch Telegram Open registrations:\nFriday 21 September 2018 | 17:00 CMT+1\n\n📝1 Resgister = 1 GGCoin ($GGC)\n🎉First 100 Resgistrations 5 $GGC\n🛄 Max: 5000 Resgistrations')
+      bot.sendMessage(msg.from.id, '🚀Launch Telegram Open registrations:\nFriday 21 September 2018 | 17:00 CMT+1\n\n📝1 Resgister = 1 GGCoin ($GGC)\n🎉First 100 Registrations 5 $GGC\n🛄 Max: 5000 $GGC')
     }else {
       bot.sendMessage(msg.from.id, "Hey "+msg.from.first_name+", I hear you want to register", {
       "reply_markup": {
